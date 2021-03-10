30594224413e6152e48f068853c9e8c3797accd2
thiago thales <tthiagothales@gmail.com>
2021-03-10 15:44:05 -0300

    just the basic

---
+++ start-me-up.sh
@@ -0,0 +1,55 @@
+sudo apt-get update
+
+echo 'installing NodeJS'
+sudo apt install nodejs
+
+echo 'installing NPM'
+sudo apt install npm
+
+echo 'installing git' 
+sudo apt install git -y
+
+git config --global user.name "thales"
+git config --global user.email tthiagothales@gmail.com
+
+echo 'installing zsh'
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+
+echo 'installing vim'
+sudo apt install vim -y
+
+
+
+echo 'installing code'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update
+sudo apt-get install code -y # or code-insiders
+
+echo 'installing docker' 
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+chmod 777 /var/run/docker.sock
+docker run hello-world
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'installing pip and libs'
+sudo apt-get intall python3-pip
+sudo pip install pandas
+sudo pip install selenium
+sudo pip install requests
+sudo pip install beautifulsoup4
+sudo pip install django
+sudo pip install numpy
